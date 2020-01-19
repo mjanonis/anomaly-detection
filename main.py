3598c148fcecfbfd696086e1aa5e23e538c65b65
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-01-19 11:27:09 +0000

    Reduced the margin to 0.5

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
 siamese_train_loader = torch.utils.data.DataLoader(siamese_train_dataset, batch_size=batch_size, shuffle=True, **kwargs)
 siamese_test_loader = torch.utils.data.DataLoader(siamese_test_dataset, batch_size=batch_size, shuffle=False, **kwargs)
 
-margin = 1.
+margin = 0.5
 embedding_net = ResNextEmbeddingNet()
 model = SiameseNet(embedding_net)
 if cuda:
