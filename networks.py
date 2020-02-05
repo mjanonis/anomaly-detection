88ed8acdfa9be0dfc18062fe2c46f8d9395773a2
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-02-05 15:01:24 +0000

    Added a DenseNet201 embedding network and made it so all embedding networks return a 1024D vector

--- networks.py
+++ networks.py
@@ -6,13 +6,38 @@ import torchvision
 class ResNextEmbeddingNet(nn.Module):
     def __init__(self):
         super(ResNextEmbeddingNet, self).__init__()
-        self.convnet = torchvision.models.resnext101_32x8d(pretrained='True')
+        self.convnet = torchvision.models.resnext101_32x8d(pretrained=True)
 
         # Strip the classification layer
         self.convnet = nn.Sequential(*(list(self.convnet.children())[:-1]))
+        # Add an avg pooling layer (need output shape of [?, 1024, 1, 1])
+        self.aavgp3d = nn.AdaptiveAvgPool3d(output_size=(1024, 1, 1))
+        self.flat = nn.Flatten()
 
     def forward(self, x):
         output = self.convnet(x)
+        output = self.aavgp3d(output)
+        output = self.flat(output)
+        return output
+
+    def get_embedding(self, x):
+        return self.forward(x)
+
+class DenseNetEmbeddingNet(nn.Module):
+    def __init__(self):
+        super(DenseNetEmbeddingNet, self).__init__()
+        self.convnet = torchvision.models.densenet201(pretrained=True)
+
+        # Strip the classification layer
+        self.convnet = nn.Sequential(*(list(self.convnet.children())[:-1]))
+        # Add an avg pooling layer (need output shape of [?, 1024, 1, 1])
+        self.aavgp3d = nn.AdaptiveAvgPool3d(output_size=(1024, 1, 1))
+        self.flat = nn.Flatten()
+
+    def forward(self, x):
+        output = self.convnet(x)
+        output = self.aavgp3d(output)
+        output = self.flat(output)
         return output
 
     def get_embedding(self, x):
