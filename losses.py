7a06acd2bdd7abfb6f8a12425d6c8c209d8ee8ce
Martynas Janonis <martynas.janonis.17@ucl.ac.uk>
2020-02-20 19:54:52 +0000

    Removed TripletLoss as PyTorch already has an implementation for it

--- losses.py
+++ losses.py
@@ -27,23 +27,6 @@ class ContrastiveLoss(nn.Module):
         return losses.mean() if size_average else losses.sum()
 
 
-class TripletLoss(nn.Module):
-    """
-    Triplet loss
-    Takes embeddings of an anchor sample, a positive sample and a negative sample
-    """
-
-    def __init__(self, margin):
-        super(TripletLoss, self).__init__()
-        self.margin = margin
-
-    def forward(self, anchor, positive, negative, size_average=True):
-        distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
-        distance_negative = (anchor - negative).pow(2).sum(1)  # .pow(.5)
-        losses = F.relu(distance_positive - distance_negative + self.margin)
-        return losses.mean() if size_average else losses.sum()
-
-
 class OnlineContrastiveLoss(nn.Module):
     """
     Online Contrastive loss
